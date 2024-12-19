82d7d81c209ea3a30282baf502914e51c60b988f
Karthik Ragula <ragulakarthik04@gmail.com>
2024-12-19 19:39:07 +0530

    intial code for parking spot booking

---
+++ src/main/java/com/quick_park_assist/service/IBookingSpotService.java
@@ -0,0 +1,7 @@
+package com.quick_park_assist.service;
+
+import com.quick_park_assist.entity.BookingSpot;
+
+public interface IBookingSpotService {
+    void saveBookingSpot(BookingSpot bookingSpot);
+}
